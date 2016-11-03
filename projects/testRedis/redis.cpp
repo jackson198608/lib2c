41eaa8941ce6e28d65e3ae27a07b37ec375e5fff
周鹏远 <zhoupengyuan@goumin.com>
2016-11-04 01:01:29 +0800

    tidy it

--- projects/testRedis/redis.cpp
+++ projects/testRedis/redis.cpp
@@ -2,14 +2,14 @@
 
 int main()
 {
- Redis *r = new Redis();
- if(!r->connect("210.14.154.198", 6379))
- {
-  printf("connect error!\n");
-  return 0;
- }
- r->set("name", "Mayuyu");
- printf("Get the name is %s\n", r->get("name").c_str());
- delete r;
- return 0;
+		Redis *r = new Redis();
+		if(!r->connect("210.14.154.198", 6379))
+		{
+				printf("connect error!\n");
+				return 0;
+		}
+		r->set("name", "Mayuyu");
+		printf("Get the name is %s\n", r->get("name").c_str());
+		delete r;
+		return 0;
 }
